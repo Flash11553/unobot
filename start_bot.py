c441d7250d18fea735cc254f4ec9973d5d835983
Flash11553 <flash.14599@gmail.com>
2025-07-31 16:35:53 +0400

    Update start_bot.py

--- start_bot.py
+++ start_bot.py
@@ -1,25 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-# Telegram bot to play UNO in group chats
-# Copyright (c) 2016 Jannes Höke <uno@jhoeke.de>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+from telegram.ext import Updater, CommandHandler
 
-# Modify this file if you want a different startup sequence, for example using
-# a Webhook
+TOKEN = "SENIN_BOT_TOKENIN"
 
+def start(update, context):
+    update.message.reply_text("Salam! Mən Heroku üzərində işləyən sadə botam.")
 
-def start_bot(updater):
+def main():
+    updater = Updater(TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+
     updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
