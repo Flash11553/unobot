a99f30204fb9e01d16a822c38bcfeca51fd8208f
Flash11553 <flash.14599@gmail.com>
2025-08-01 16:54:53 +0400

    Update internationalization.py

--- internationalization.py
+++ internationalization.py
@@ -41,7 +41,7 @@ class _Underscore(object):
             )
             for locale
             in available_locales.keys()
-            if locale != 'en_US'  # No translation file for en_US
+            if locale != 'az_AZ'  # No translation file for az_Az
         }
         self.locale_stack = list()
 
