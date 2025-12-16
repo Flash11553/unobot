a8bedcd847853c9a173e948895f84db014ce8ad3
Flash11553 <flash.14599@gmail.com>
2025-12-16 18:54:32 +0400

    Update simple_commands.py

--- simple_commands.py
+++ simple_commands.py
@@ -43,20 +43,20 @@ context.dispatcher.run_async(_send)
 
 @user_locale
 def modes(update: Update, context: CallbackContext):
-"""Handler for the /help command"""
-modes_explanation = _("This UNO bot has four game modes: Classic, Sanic, Wild and Text.\n\n"
-" 🎻 The Classic mode uses the conventional UNO deck and there is no auto skip.\n"
-" 🚀 The Sanic mode uses the conventional UNO deck and the bot automatically skips a player if he/she takes too long to play its turn\n"
-" 🐉 The Wild mode uses a deck with more special cards, less number variety and no auto skip.\n"
-" ✍️ The Text mode uses the conventional UNO deck but instead of stickers it uses the text.\n\n"
-"To change the game mode, the GAME CREATOR has to type the bot nickname and a space, "
-"just like when playing a card, and all gamemode options should appear.")
-send_async(context.bot, update.message.chat_id, text=modes_explanation,
-parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+    """Handler for the /help command"""
+    modes_explanation = _("This UNO bot has four game modes: Classic, Sanic, Wild and Text.\n\n"
+      " 🎻 The Classic mode uses the conventional UNO deck and there is no auto skip.\n"
+      " 🚀 The Sanic mode uses the conventional UNO deck and the bot automatically skips a player if he/she takes too long to play its turn\n"
+      " 🐉 The Wild mode uses a deck with more special cards, less number variety and no auto skip.\n"
+      " ✍️ The Text mode uses the conventional UNO deck but instead of stickers it uses the text.\n\n"
+      "To change the game mode, the GAME CREATOR has to type the bot nickname and a space, "
+      "just like when playing a card, and all gamemode options should appear.")
+   send_async(context.bot, update.message.chat_id, text=modes_explanation,
+              parse_mode=ParseMode.HTML, disable_web_page_preview=True) 
 
 @user_locale
 def source(update: Update, context: CallbackContext):
-"""Handler for the /help command"""
+    """Handler for the /help command"""
 source_text = _("This bot is Free Software and licensed under the AGPL. "
 "The code is available here: \n"
 "https://github.com/jh0ker/mau_mau_bot")
@@ -74,14 +74,14 @@ send_async(context.bot, update.message.chat_id, text=source_text + '\n' +
 
 @user_locale
 def news(update: Update, context: CallbackContext):
-"""Handler for the /news command"""
+    """Handler for the /news command"""
 send_async(context.bot, update.message.chat_id,
 text=_("All news here: https://telegram.me/unobotnews"),
 disable_web_page_preview=True)
 
 @user_locale
 def stats(update: Update, context: CallbackContext):
-user = update.message.from_user
+    user = update.message.from_user
 us = UserSetting.get(id=user.id)
 if not us:
 send_async(context.bot, update.message.chat_id,
